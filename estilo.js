74b1dc2245bee5823e9f48c67b916a20de79cdff
jbuezo0 <jbuezo0@gmail.com>
2020-04-03 17:14:18 -0600

    Putos de cierr

---
+++ estilo.js
@@ -0,0 +1,27 @@
+
+$(document).ready(function() {
+
+    $(".navdrop").click(function() {
+    
+      $("nav").slideToggle(200);	
+    
+        $(this).toggleClass("close");
+                                     
+      }); 
+    
+    
+    $(".plus").click(function(e) {
+    
+         e.preventDefault();
+    
+         var current_width = $(window).width();
+    
+         if(current_width < 860) {
+    
+      $(this).toggleClass("minus").next('nav li ul').slideToggle();
+     
+        }
+    
+      });
+    
+    }); // end document ready
